8119f8e13a358d3a7ee584908a2663c69f7fde2f
Pavel <pav.konoval@gmail.com>
2023-02-25 16:49:27 +0200

    feat: greeting

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 const time = document.querySelector('.time');
 const data = document.querySelector('.date');
+const greeting = document.querySelector('.greeting');
+const input = document.querySelector('.name');
+// 1
 
 function showDate() {
 	const date = new Date();
@@ -20,6 +23,43 @@ function showTime() {
 	time.textContent = currentTime;
 	setTimeout(showTime, 1000);
 	showDate();
+	showGreeting();
 }
 
 showTime();
+
+/**
+================================>	2 Greeting 
+*/
+
+function showGreeting() {
+	function getTimeOfDay() {
+		const date = new Date();
+		const hours = date.getHours();
+		if (hours < 6) {
+			return 'night';
+		} else if (hours < 12) {
+			return 'morning';
+		} else if (hours < 18) {
+			return 'afternoon';
+		} else {
+			return 'evening';
+		}
+	}
+
+	const timeOfDay = getTimeOfDay();
+	const greetingText = `Good ${timeOfDay}`;
+	greeting.textContent = greetingText;
+}
+
+function setLocalStorage() {
+	localStorage.setItem('name', input.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+	if (localStorage.getItem('name')) {
+		input.value = localStorage.getItem('name');
+	}
+}
+window.addEventListener('load', getLocalStorage);
