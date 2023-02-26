74aa8b39b4fb4d69b3f997797663103d13807fee
Pavel <pav.konoval@gmail.com>
2023-02-26 22:16:37 +0200

    fix: mp3 to wav

--- js/playList.js
+++ js/playList.js
@@ -1,22 +1,22 @@
 const playList = [
 	{
 		title: 'Aqua Caelestis',
-		src: '/assets/sounds/Aqua Caelestis.mp3',
+		src: '/assets/sounds/Aqua Caelestis.wav',
 		duration: '00:39',
 	},
 	{
 		title: 'River Flows In You',
-		src: '/assets/sounds/River Flows In You.mp3',
+		src: '/assets/sounds/River Flows In You.wav',
 		duration: '01:37',
 	},
 	{
 		title: 'Ennio Morricone',
-		src: '/assets/sounds/Ennio Morricone.mp3',
+		src: '/assets/sounds/Ennio Morricone.wav',
 		duration: '01:37',
 	},
 	{
 		title: 'Summer Wind',
-		src: '/assets/sounds/Summer Wind.mp3',
+		src: '/assets/sounds/Summer Wind.wav',
 		duration: '01:50',
 	},
 ];
